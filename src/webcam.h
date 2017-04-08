e8bc76a9702b3457f3720592f3c254f565af94e2
Alexandru Csete <oz9aec@gmail.com>
2017-04-09 00:15:10 +0200

    Indent webcam

--- src/webcam.h
+++ src/webcam.h
@@ -1,42 +1,43 @@
-/** Small C++ wrapper around V4L example code to access the webcam
-**/
-
-#include <string>
-#include <memory> // unique_ptr
+/*
+ * Small C++ wrapper around V4L example code to access the webcam
+ */
 #include <linux/videodev2.h>
+#include <memory>              // unique_ptr
+#include <string>
+
 struct buffer {
-      void   *data;
-      size_t  size;
+    void     *data;
+    size_t    size;
 };
 
 struct YUV420Image {
-      unsigned char   *data; // YUV420
-      size_t          width;
-      size_t          height;
-      size_t          size; // width * height * 3/2
+    unsigned char   *data;     // YUV420
+    size_t           width;
+    size_t           height;
+    size_t           size;     // width * height * 3/2
 };
 
-
-class Webcam {
-
+class Webcam
+{
 public:
-    Webcam(const std::string& device = "/dev/video0");
+    Webcam(const std::string &device = "/dev/video0");
 
     ~Webcam();
 
-    void GetCameraSize(int& Width,int& Height);
-	int ConvertColor(unsigned char *out,unsigned char *in);
-    void SetOmxBuffer(unsigned char* Buffer);
-    /** Captures and returns a frame from the webcam.
+    void GetCameraSize(int &Width, int &Height);
+    int ConvertColor(unsigned char *out, unsigned char *in);
+    void SetOmxBuffer(unsigned char *Buffer);
+
+    /* Captures and returns a frame from the webcam.
      *
      * The returned object contains a field 'data' with the image data in YUV420
-     * format 
+     * format
      * This call blocks until a frame is available or until the provided
-     * timeout (in seconds). 
+     * timeout (in seconds).
      *
      * Throws a runtime_error if the timeout is reached.
      */
-    const YUV420Image& frame(int timeout = 1);
+    const YUV420Image &frame(int timeout = 1);
 
 private:
     void init_mmap();
@@ -56,16 +57,12 @@ private:
     int fd;
 
     YUV420Image yuv420frame;
-    struct buffer          *buffers;
-    unsigned int     n_buffers;
+    struct buffer *buffers;
+    unsigned int n_buffers;
 
     size_t xres, yres;
     size_t stride;
     struct v4l2_format fmt;
     bool force_format = false;
-    bool StatusCapturing=false;
+    bool StatusCapturing = false;
 };
-
-
-
-
